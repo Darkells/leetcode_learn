635f6156b3ce1b95d45e1648e6f22500a991bc14
君莫笑 <951086294@qq.com>
2019-03-16 00:24:34 +0800

    3.15日晚上的题目

---
+++ src/Easy/ListNode.java
@@ -0,0 +1,13 @@
+package Easy;
+
+/**
+ * @author Dark
+ * @date 2019/3/14 22:25
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x){
+        val = x;
+    }
+}
---
+++ src/Primary_code/String/StrStr.java
@@ -0,0 +1,14 @@
+package Primary_code.String;
+
+/**
+ * @author Dark
+ * @date 2019/3/12 13:30
+ */
+public class StrStr {
+    public static int strStr(String haystack, String needle){
+        return haystack.indexOf(needle);
+
+    }
+
+
+}
