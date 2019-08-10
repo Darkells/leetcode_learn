69be290df9157cd748f33c917e4c9be6cb4490c1
Darkells <951086294@qq.com>
2019-08-10 15:37:05 +0800

    08.10

---
+++ src/Medium/IntToRoman.java
@@ -0,0 +1,25 @@
+package Medium;
+
+/**
+ *
+ */
+public class IntToRoman {
+    public String intToRoman(int num) {
+        int[] nums = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder sb = new StringBuilder();
+
+        int index = 0;
+
+        while(index < 13) {
+            while (num >= nums[index]) {
+                sb.append(romans[index] + "");
+                num -= nums[index];
+            }
+            index++;
+        }
+
+        return sb.toString();
+    }
+}
