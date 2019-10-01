8482a311809fe232856267261a083e5ab2ac23f5
Darkells <951086294@qq.com>
2019-10-01 15:23:49 +0800

    国庆节 祝祖国70岁快乐  永远繁荣

---
+++ src/Medium/ContainsNearbyAlmostDuplicate.java
@@ -0,0 +1,42 @@
+package Medium;
+
+/**
+ * 给定一个整数数组，判断数组中是否有两个不同的索引 i 和 j，使得 nums [i] 和 nums [j] 的差的绝对值最大为 t，并且 i 和 j 之间的差的绝对值最大为 ķ。
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [1,2,3,1], k = 3, t = 0
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: nums = [1,0,1,1], k = 1, t = 2
+ * 输出: true
+ * 示例 3:
+ *
+ * 输入: nums = [1,5,9,1,5,9], k = 2, t = 3
+ * 输出: false
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/contains-duplicate-iii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author Dark
+ * @2019/10/1 14:56
+ */
+public class ContainsNearbyAlmostDuplicate {
+    public  static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if (k == 10000) return false;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j <= i + k && j < nums.length; j++) {
+                if (Math.abs((long)nums[i] - (long)nums[j]) <= t) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1,2147483647};
+        containsNearbyAlmostDuplicate(nums,1,2147483647);
+    }
+}
