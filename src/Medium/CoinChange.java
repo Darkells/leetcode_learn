2182d95281e77fab23fdd3e26f9b8cd0df9dbe9e
Darkells <951086294@qq.com>
2019-10-16 19:14:47 +0800

    10.16

---
+++ src/Medium/CoinChange.java
@@ -0,0 +1,23 @@
+package Medium;
+
+import java.util.Arrays;
+
+/**
+ * @author Dark
+ * @2019/10/16 15:33
+ */
+public class CoinChange {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp,amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
