7b7c71715a92f4eb40c6e79adeb8a6e343117f2d
君莫笑 <951086294@qq.com>
2019-11-09 13:27:53 +0800

    11.07

---
+++ src/Medium/KthSmallestMatrix.java
@@ -0,0 +1,59 @@
+package Medium;
+
+/**
+ * 给定一个 n x n 矩阵，其中每行和每列元素均按升序排序，找到矩阵中第k小的元素。
+ * 请注意，它是排序后的第k小元素，而不是第k个元素。
+ *
+ * 示例:
+ *
+ * matrix = [
+ *    [ 1,  5,  9],
+ *    [10, 11, 13],
+ *    [12, 13, 15]
+ * ],
+ * k = 8,
+ *
+ * 返回 13。
+ * 说明:
+ * 你可以假设 k 的值永远是有效的, 1 ≤ k ≤ n2 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/kth-smallest-element-in-a-sorted-matrix
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class KthSmallestMatrix {
+    public int kthSmallestMatrix(int[][] matrix, int k) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int left = matrix[0][0];
+        int right = matrix[row - 1][col - 1];
+        while (left < right) {
+            int mid = (left + right) >> 1;
+            int count = Count(matrix, mid, row, col);
+
+            if (count < k) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return right;
+    }
+
+    private int Count(int[][] matrix, int mid, int row, int col) {
+        int i = row - 1;
+        int j = 0;
+        int count = 0;
+        while (i >= 0 && j < col) {
+            if (matrix[i][j] <= mid) {
+                count += i + 1;
+                j++;
+            } else {
+                i--;
+            }
+        }
+
+        return count;
+    }
+}
